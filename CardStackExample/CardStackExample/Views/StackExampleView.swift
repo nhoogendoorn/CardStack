1d1295c86e02108834d8910fa774f735e2587d0b
Niels Hoogendoorn <niels@appical.nl>
2020-03-15 15:46:28 +0100

    - added example project files - renaming of cardstack files

---
+++ CardStackExample/CardStackExample/Views/StackExampleView.swift
@@ -0,0 +1,29 @@
+//
+//  StackExampleView.swift
+//  CardStackExample
+//
+//  Created by Niels Hoogendoorn on 15/03/2020.
+//  Copyright © 2020 Nihoo. All rights reserved.
+//
+
+import SwiftUI
+import CardStack
+
+struct StackExampleView: View {
+    let items: [DataExample] = [DataExample(id: UUID().uuidString, color: .red),
+                 DataExample(id: UUID().uuidString, color: .blue),
+                 DataExample(id: UUID().uuidString, color: .yellow),
+                 DataExample(id: UUID().uuidString, color: .green),
+                 DataExample(id: UUID().uuidString, color: .orange)
+    ]
+    
+    var body: some View {
+        MainContainer<CardItemView, InfoExample>(configuration: nil, items: items)
+    }
+}
+
+struct StackExampleView_Previews: PreviewProvider {
+    static var previews: some View {
+        StackExampleView()
+    }
+}
--- Sources/CardStack/Views/CardStack.swift
+++ Sources/CardStack/Views/CardStack.swift
@@ -10,64 +10,19 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public struct CardStack<Content: Card, CardData: CardInformation>: View {
-    @EnvironmentObject var navigation: CardNavigation<CardData>
-    @State var dragValue: CGPoint = .zero
     var configuration: StackConfiguration?
-    
-    init(configuration: StackConfiguration? = nil) {
+    let items: [CardData]
+
+    init(configuration: StackConfiguration? = nil, items: [CardData]) {
         self.configuration = configuration
+        self.items = items
     }
     
+    @available(OSX 10.15.0, *)
     public var body: some View {
-        ZStack(alignment: .trailing) {
-            // Reverse the item list so the first item in the list
-            // will show in front.
-            ForEach(navigation.reversedList, id: \.id) { item in
-                CardView<Content, CardData>(info: item,
-                                            configuration: self.configuration ?? StackConfiguration.shared,
-                                            dragValue: self.$dragValue)
-            }
-        }.frame(maxWidth: .infinity, maxHeight: .infinity)
-            // We need to adjust the offset for the current index after flipping
-            // the cards otherwise it will stay in the same position. We want to
-            // have every list item on the place of the first index in the list.
-            .offset(x: getOffsetForStackedView(), y: .zero)
-            .gesture(DragGesture()
-                .onChanged({ (value: DragGesture.Value) in
-                    self.handleHorizontalDragging(value)
-                })
-                .onEnded({ (value: DragGesture.Value) in
-                    self.handleDraggingEnd(value)
-                })
-        )
-    }
-    
-    fileprivate func getOffsetForStackedView() -> CGFloat {
-        return CardViewHelper.getListOffset(currentIndex: navigation.currentIndex)
-    }
-    
-    fileprivate func handleHorizontalDragging(_ value: DragGesture.Value) {
-        self.dragValue = CGPoint(x: value.horizontalDistance(), y: .zero)
-    }
-    
-    fileprivate func handleDraggingEnd(_ value: DragGesture.Value) {
-        self.handleSwipeDirection(value)
-        
-        // Reset dragvalue after moving so that the card is at its base position.
-        self.dragValue = .zero
-    }
-    
-    fileprivate func handleSwipeDirection(_ value: DragGesture.Value) {
-        let minDistance = configuration?.minimumSwipingDistance ?? StackConfiguration.shared.minimumSwipingDistance
-        
-        let swipeDirection = value.getSwipeDirection(minimumDistance: minDistance)
-        switch swipeDirection {
-        case .forward:
-            self.navigation.move(.forward)
-        case .backward:
-            self.navigation.move(.backward)
-        case .none:
-            break
-        }
+        CardStackMainView<Content, CardData>(configuration: configuration)
+            .animation(.default)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .environmentObject(CardNavigation(items: items))
     }
 }
